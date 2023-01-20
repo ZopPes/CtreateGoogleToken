3999964854b5d9a8627b03855b0165f6dc747bdb
kahtanov <Zop859@mail.ru>
2023-01-20 17:31:02 +0300

    Добавьте файлы проекта.

---
+++ CtreateGoogleToken/Program.cs
@@ -0,0 +1,20 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Util.Store;
+
+Console.WriteLine("путь к клиенту Google");
+var clientPath=Console.ReadLine();
+
+UserCredential Client = await GoogleWebAuthorizationBroker.AuthorizeAsync
+       (
+           clientSecrets: (await GoogleClientSecrets.FromFileAsync(clientPath)).Secrets
+           ,
+           scopes: new[] { SheetsService.Scope.Spreadsheets }
+           ,
+           user: "user"
+           ,
+           taskCancellationToken: CancellationToken.None
+           ,
+           new FileDataStore("./", true)
+           );
+Console.WriteLine($"Token: {Client.Token.RefreshToken}");
